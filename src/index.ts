23d7b2384f4f55a1d742a55b672225ebb482baa6
Levente Kiss <levente.kiss@solarpunk.bzz>
2024-12-19 18:06:12 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { SwarmChat } from './libs/core';
+
+export { EVENTS } from './libs/constants';
+
+export type { MessageData, UserWithIndex, EthAddress, Bees, VisibleMessage } from './libs/types';
---
+++ src/libs/constants.ts
@@ -0,0 +1,17 @@
+export const CONSENSUS_ID = 'SwarmStream';
+
+export const EVENTS = {
+  LOADING_INIT_USERS: 'loadingInitUsers',
+  MESSAGE_RECEIVED: 'messageReceived',
+  MESSAGE_REQUEST_SENT: 'messageRequestSent',
+  MESSAGE_REQUEST_ERROR: 'messageRequestError',
+  ERROR: 'errorEvent',
+};
+
+export const SECOND = 1000;
+export const MINUTE = SECOND * 60;
+export const HOUR = MINUTE * 60;
+
+export const HEX_RADIX = 16;
+
+export const ETH_ADDRESS_LENGTH = 42;
